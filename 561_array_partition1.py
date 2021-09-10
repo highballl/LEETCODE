533a2ec2c0aa60917a430cf0a301537e24f31d16
parkjiyoon <jyp0293@gmail.com>
2021-09-10 11:29:47 +0900

    add: 561_array_partition1.py

---
+++ 561_array_partition1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        result = 0
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                result += nums[i]
+        
+        return result
